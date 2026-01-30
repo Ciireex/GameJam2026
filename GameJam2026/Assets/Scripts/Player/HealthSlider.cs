64e11714c4f1c2bf16050b70491f152f14f75ec3
Guillem Cadevall Ferreres <guillecade22@gmail.com>
2026-01-30 20:21:49 +0100

    player basic

--- GameJam2026/Assets/Scripts/Player/HealthSlider.cs
+++ GameJam2026/Assets/Scripts/Player/HealthSlider.cs
@@ -1,45 +1,68 @@
-using System;
+//using System;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+//using UnityEngine.UI;
+
+//public class HealthSlider : MonoBehaviour
+//{
+//    [SerializeField] private Slider slider;
+//    [SerializeField] private float maxValueInSeconds = 15f;
+//    [SerializeField] private bool countdownActive = false;
+//    public event Action OnHealthDepleted;
+
+
+
+//    void Start()
+//    {
+//        slider.maxValue = maxValueInSeconds;
+//        slider.value = maxValueInSeconds;
+//        GameInput.Instance.OnChangeMaskAction += GameInput_OnChangeMaskAction;
+
+//    }
+
+//    private void GameInput_OnChangeMaskAction(object sender, EventArgs e)
+//    {
+//        ToggleCountdown();
+//    }
+
+//    void Update()
+//    {
+//        if (countdownActive)
+//            slider.value -= Time.deltaTime;
+//    }
+
+//    public void ToggleCountdown()
+//    {
+//        countdownActive = !countdownActive;
+//    }
+
+
+//    public void OnHealthChanged()
+//    {
+//        if (slider.value <= slider.minValue)
+//        {
+//            Debug.Log("Player has died");
+//            OnHealthDepleted?.Invoke();
+//            countdownActive = false;
+//        }
+//    }
+//}
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class HealthSlider : MonoBehaviour
 {
     [SerializeField] private Slider slider;
-    [SerializeField] private float maxValueInSeconds = 15f;
-    public event Action OnHealthDepleted;
-    private bool countdownActive = true;
 
-
-    void Start()
+    public void SetMaxValue(float max)
     {
-        slider.maxValue = maxValueInSeconds;
-        slider.value = maxValueInSeconds;
+        slider.maxValue = max;
+        slider.value = max;
     }
 
-    void Update()
+    public void SetValue(float value)
     {
-        if (countdownActive)
-            slider.value -= Time.deltaTime;
-    }
-
-    public void ActivateCountdown()
-    {
-        countdownActive = true;
-    }
-
-    public void DeactivateCountdown()
-    {
-        countdownActive = false;
-    }
-
-    public void OnHealthChanged()
-    {
-        if (slider.value <= slider.minValue)
-        {
-            Debug.Log("Player has died");
-            OnHealthDepleted?.Invoke();
-            countdownActive = false;
-        }
+        slider.value = value;
     }
 }
+
--- GameJam2026/Assets/Scripts/Player/Player.cs
+++ GameJam2026/Assets/Scripts/Player/Player.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
-
+using System;
 public class Player : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private float health = 100f;
+    [SerializeField] private float maxHealthTime = 15f;
+    [SerializeField] private HealthSlider healthSlider;
 
+    public event EventHandler OnPlayerDeath;
+
+    private float currentHealthTime;
     private bool isMaskOn = true;
-    
+    private bool countdownActive;
+
 
     private void Start()
     {
+        currentHealthTime = maxHealthTime;
+        healthSlider.SetMaxValue(maxHealthTime);
         GameInput.Instance.OnChangeMaskAction += GameInput_OnChangeMaskAction;
-    }
-
-    private void GameInput_OnChangeMaskAction(object sender, System.EventArgs e)
-    {
-        Debug.Log("Change Mask Action triggered");
     }
 
     private void Update()
     {
         HandleMovment();
+        HandleHealthCountdown();
     }
 
     private void HandleMovment() 
@@ -32,8 +35,43 @@ public class Player : MonoBehaviour
 
         transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
+
+    private void HandleHealthCountdown()
+    {
+        if (!countdownActive)
+            return;
+
+        currentHealthTime -= Time.deltaTime;
+        healthSlider.SetValue(currentHealthTime);
+
+        if (currentHealthTime <= 0f)
+        {
+            currentHealthTime = 0f;
+            countdownActive = false;
+            Debug.Log("Player has died");
+            OnPlayerDeath?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    private void GameInput_OnChangeMaskAction(object sender, System.EventArgs e)
+    {
+        ToggleMask();
+        ToggleCountdown();
+        Debug.Log("Mask is " + IsMaskOn());
+    }
+
     private bool IsMaskOn()
     {
         return isMaskOn;
+    }
+
+    public void ToggleMask()
+    {
+        isMaskOn = !isMaskOn;
+    }
+
+    private void ToggleCountdown()
+    {
+        countdownActive = !countdownActive;
     }
 }
