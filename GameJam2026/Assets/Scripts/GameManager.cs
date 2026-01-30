f59e1454ada36affcd5392448028c9e3a507d4b4
Manel Palacín <121304227+manelpalacdi@users.noreply.github.com>
2026-01-30 23:02:40 +0100

    Added scenecontroller ref to gamemanager

--- GameJam2026/Assets/Scripts/GameManager.cs
+++ GameJam2026/Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     [SerializeField] Player player;
     public Player Player => player;
+    [SerializeField] private SceneController sceneController;
+    public SceneController SceneController => sceneController;
 
     private void Awake()
     {
