d7c881ab7c7cf0c20949646dbdb05f5dc3d6ec4b
Manel Palacín <121304227+manelpalacdi@users.noreply.github.com>
2026-01-31 20:59:00 +0100

    toxic area now plays geiger, ipdated prefab and code

--- GameJam2026/Assets/Scripts/ToxicArea.cs
+++ GameJam2026/Assets/Scripts/ToxicArea.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
@@ -7,16 +9,27 @@ public class ToxicArea : MonoBehaviour
     [SerializeField] private float healthDrainSpeedMultiplier = 3f;
     private ColorCurves curves;
     private Volume volume;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip geigerSFX;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void Start()
     {
         GameObject volumeGO = GameObject.FindGameObjectWithTag("URP");
         volume = volumeGO.GetComponent<Volume>();
         volume.profile.TryGet(out curves);
+        audioSource.clip = geigerSFX;
         Debug.Log(curves);
     }
 
     void OnTriggerStay2D(Collider2D collision)
     {
+        audioSource.enabled = true;
+
         if (collision.gameObject.CompareTag("Player"))
         {
             if (!GameManager.Instance.Player.IsMaskOn())
@@ -24,10 +37,17 @@ public class ToxicArea : MonoBehaviour
                 GameManager.Instance.Player.SetHealthDrainSpeedMultiplier(healthDrainSpeedMultiplier);
                 if (curves != null)
                     curves.active = true;
+                audioSource.outputAudioMixerGroup = null;
             }
             else {
                 if (curves != null)
                     curves.active = false;
+                if (audioSource.outputAudioMixerGroup == null)
+                {
+                    AudioMixerGroup mixerGroup = AudioManager.Instance.AudioMixer.FindMatchingGroups("SFX").FirstOrDefault();
+                    if (mixerGroup != null)
+                        audioSource.outputAudioMixerGroup = mixerGroup;
+                }
             }
         }
     }
@@ -39,6 +59,7 @@ public class ToxicArea : MonoBehaviour
             GameManager.Instance.Player.SetHealthDrainSpeedMultiplier(1f);
             if (curves != null)
                 curves.active = false;
+            audioSource.enabled = false;
         }
     }
 }
