6f69fb76df020557deb85e0a5b158402692b6ba1
Guillem Cadevall Ferreres <guillecade22@gmail.com>
2026-01-31 20:31:02 +0100

    push verd en nubol

--- GameJam2026/Assets/Scripts/ToxicArea.cs
+++ GameJam2026/Assets/Scripts/ToxicArea.cs
@@ -1,14 +1,34 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class ToxicArea : MonoBehaviour
 {
     [SerializeField] private float healthDrainSpeedMultiplier = 3f;
+    private ColorCurves curves;
+    private Volume volume;
+    private void Start()
+    {
+        GameObject volumeGO = GameObject.FindGameObjectWithTag("URP");
+        volume = volumeGO.GetComponent<Volume>();
+        volume.profile.TryGet(out curves);
+        Debug.Log(curves);
+    }
+
     void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             if (!GameManager.Instance.Player.IsMaskOn())
+            {
                 GameManager.Instance.Player.SetHealthDrainSpeedMultiplier(healthDrainSpeedMultiplier);
+                if (curves != null)
+                    curves.active = true;
+            }
+            else {
+                if (curves != null)
+                    curves.active = false;
+            }
         }
     }
 
@@ -17,6 +37,8 @@ public class ToxicArea : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             GameManager.Instance.Player.SetHealthDrainSpeedMultiplier(1f);
+            if (curves != null)
+                curves.active = false;
         }
     }
 }
