3991928fb2c0fee301238e05176d6ab89a25b911
Guillem Cadevall Ferreres <guillecade22@gmail.com>
2026-01-31 15:24:41 +0100

    Wipe Effect

---
+++ GameJam2026/Assets/Shaders/CircleWipe/WipeController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WipeController : MonoBehaviour
+{
+    private Animator _animatior;
+    private Image _image;
+    private readonly int _circleSizeID = Shader.PropertyToID("_CircleSize");
+
+    public float circleSize = 0f;
+
+    private bool isIn;
+
+    private void Start()
+    {
+        _animatior = gameObject.GetComponent<Animator>();
+        _image = gameObject.GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        _image.materialForRendering.SetFloat(_circleSizeID, circleSize);
+    }
+
+    public void AnimateIn()
+    {
+        _animatior.SetTrigger("In");
+        isIn = true;
+    }
+
+    public void AnimateOut()
+    {
+        _animatior.SetTrigger("Out");
+        isIn = false;
+    }
+
+    public bool IsIn() 
+    {
+        return isIn;
+    }
+}
